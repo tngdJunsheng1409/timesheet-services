f025e919376351820e3f1e955ce74e63affb2892
Choo Jun Sheng <001409@TNGD-TECH-CJS.local>
2026-02-08 15:26:07 +0800

    feat: add vercel json

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 import express, { json } from "express";
 import serverless from "serverless-http";
-import { registerAllRoutes } from "../src/routes";
+import { registerAllRoutes } from "./routes";
 
 const app = express();
 
